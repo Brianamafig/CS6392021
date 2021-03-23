e7583fcda540e85f409f8da8480c3ff32b817d58
BrianaFigueroa <bf37913n@pace.edu>
2021-03-23 02:42:16 -0400

    MinSDK:16|TargetSDK:30 Some working not all

--- EmployeeDatabase/app/src/main/java/com/example/employeedatabase/Employees.java
+++ EmployeeDatabase/app/src/main/java/com/example/employeedatabase/Employees.java
@@ -4,17 +4,31 @@ public class Employees {
     private String FirstName;
     private String LastName;
 
+    Employees(){
+        this.LastName = "empty";
+        this.FirstName = "empty";
+    }
+
 
     Employees(String FN, String LN){ // I am running out of names and sanity
         this.FirstName = FN;
         this.LastName = LN;
     }
 
-    public String EmFirstname() {
+    public String getFirstName() {
         return FirstName;
     }
 
-    public String EmLastName() {
+    public String getLastName() {
         return LastName;
     }
+
+    public void setFirstName(String FirstName){
+        this.FirstName = FirstName;
+    }
+
+    public void setLastName(String LastName){
+        this.LastName = LastName;
+    }
+
 }
--- EmployeeDatabase/app/src/main/java/com/example/employeedatabase/MainActivity.java
+++ EmployeeDatabase/app/src/main/java/com/example/employeedatabase/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import com.google.firebase.database.DataSnapshot;
@@ -14,42 +15,43 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.security.PrivateKey;
-import java.util.ArrayList;
+import java.security.SecureRandom;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mFirstname;
-    private EditText mLastname;
-    private TextView mAllName;
-    //private DatabaseReference ChildCount;
+    public EditText mFirstname;
+    public EditText mLastname;
+    public TextView mAllName;
+    private DatabaseReference myRef;
     private int mcount = 0;
-    //private String[] First;
-    //private String[] Second;
-    private ArrayList<String> First;
-    private ArrayList<String> Last;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mFirstname = findViewById(R.id.FirstName);
         mLastname = findViewById(R.id.LastName);
         mAllName = findViewById(R.id.AllName);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("employees");
+        myRef = database.getReference().child("employees");
 
 
-        /*myRef.addValueEventListener(new ValueEventListener() {
+        myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String string = "";
+                Employees emp;
+                int counter = 0;
                 for(DataSnapshot snapshot2 : snapshot.getChildren()){
-                    Employees employees = snapshot2.getValue(Employees.class);
-                    First.add(employees.EmFirstname());
-                    Last.add(employees.EmLastName());
+                    emp =(Employees)snapshot2.getValue(Employees.class);
+                    string = string + "First Name: "+ emp.getFirstName()+ ", Last Name: " + emp.getLastName() + "\n";
+                    Log.i("MAIN ACTIVITY",counter + "Firstname = "+emp.getFirstName()+" Lastname = " + emp.getLastName());
+                    counter++;
                 }
+                mAllName.setText(string);
+                mcount = counter;
                 //Employees employees = snapshot.getValue(Employees.class);
 
 
@@ -61,13 +63,22 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("The Read Failed" + error.getCode());
 
             }
-        });*/
+        });
 
         //ChildCount = FirebaseDatabase.getInstance().getReference().child("employees");
         //Log.i("MAINACTIVITY", myRef.toString());
 
         //Log.i("MAINACTIVITY", ChildCount.toString());
+    }
+
+    public void AddEmployee(View view) {
+        String fn = mFirstname.getText().toString();
+        String Ln = mLastname.getText().toString();
+        Employees emp = new Employees(fn, Ln);
+        //myRef.push().setValue(emp);
+        myRef.child(Integer.toString(mcount)).setValue(emp);
     }
 }
